20a384a9efdf63c82c773690364d9266d2866a9f
Veniamin benjamin Kleshchenkov <benjamin@yandex-team.ru>
2011-12-29 04:43:51 +0400

    index page

---
+++ pages/index/index.bemjson.js
@@ -0,0 +1,21 @@
+({
+    block: 'b-page',
+    title: 'Верстка. Домашние задания',
+    head: [
+        { elem: 'css', url: 'index.css'}
+    ],
+    content: {
+        block: 'b-menu-vert',
+        title: 'Верстка. Домашние задания',
+        content: [
+            {
+                elem: 'item',
+                content: {
+                    block: 'b-link',
+                    url: '/pages/rating/rating.html',
+                    content: 'Рейтинг'
+                }
+            }
+        ]
+    }
+})
